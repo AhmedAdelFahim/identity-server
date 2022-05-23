40dff96777d97d43dcd4fca4df23bb4997f63c04
Ahmed Adel <ahmedadelfahim@gmail.com>
2022-05-23 10:48:37 +0200

    Update index.js

--- src/test/index.js
+++ src/test/index.js
@@ -1,12 +1,4 @@
 /* eslint-disable require-jsdoc */
-// import {startDbConnection} from '../../config/db-connection';
-// import JobScheduler from '../utils/job-scheduler/job-scheduler';
-// import {checkURL} from '../modules/url/url.service';
-// import {Redis} from '../utils/redis';
-// import mongoose from 'mongoose';
-// import {initializeUsersForTesting} from './user-helper';
-// import {initializeURLForTesting} from './url-helper';
-// import {initializeLogsForTesting} from './report-helper';
 const Redis = require("../utils/redis");
 const { startDbConnection } = require("../config/db-connection");
 const { default: mongoose } = require("mongoose");
